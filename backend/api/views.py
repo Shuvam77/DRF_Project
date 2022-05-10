ac18207ba27b115e512278f6bd2568569e091685
shuvam77 <shuvam77@gmail.com>
2022-05-10 18:20:16 +0200

    GET Data basic

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,22 @@
+import json
 from django.http import JsonResponse
 
 # Create your views here.
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message":"Hi There, I am learning from CodingEntrepreneurs!!"})
+
+    print(request.GET) #url query params
+    body = request.body #byte string of JSON data
+    data = {}
+    try:
+        data=json.loads(body) #String of JSON data -> Python Dict
+    except:
+        pass
+    print(data.keys())
+    print(data)
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+
+    return JsonResponse(data)
+    # return JsonResponse({"message":"Hi There, I am learning from CodingEntrepreneurs!!"})
--- py_client/basic.py
+++ py_client/basic.py
@@ -7,7 +7,7 @@ endpoint = "http://127.0.0.1:8000/"
 #HTTP Request
 get_response = requests.get(endpoint, params ={"abc":123}, json={"query": "Hello World!"})
 #Print response from request in text form
-print(get_response.text)
+# print(get_response.text)
  
 print(get_response.json())
 print(get_response.status_code)
