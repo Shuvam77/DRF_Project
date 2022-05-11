dad2288f462cf5d6357343a7708c222db2da853a
shuvam77 <shuvam77@gmail.com>
2022-05-11 16:07:47 +0200

    intregate faker

---
+++ backend/api/management/commands/createdata.py
@@ -0,0 +1,73 @@
+from django.core.management.base import BaseCommand
+from faker import Faker
+import faker.providers
+from api.models import Product
+import random
+
+Content = [
+    "JavaScript",
+    "Django",
+    "Docker",
+    "Python",
+    "Postman",
+    "Git",
+    "HTML/CSS",
+    "Bootstrap",
+    "React",
+    "Node.js",
+    "Java",
+    "Ajax",
+    "JQuery"
+]
+
+Title = [
+    "Soap",
+    "Mobile",
+    "Car",
+    "Bike",
+    "Motor",
+    "Chocolate",
+    "Biscuits",
+    "Cake",
+    "Bottle",
+    "Spoon",
+    "Banana",
+    "Handy",
+    "Book",
+    "Notebook",
+    "Chair",
+    "Towel",
+    "Door",
+    "bag",
+    "Suitcase"
+]
+
+class Provider(faker.providers.BaseProvider):
+    def product_content(self):
+        return self.random_element(Content)
+    def product_title(self):
+        return self.random_element(Title)
+    def price(self):
+        price = random.uniform(10,100)
+        return price
+
+class Command(BaseCommand):
+    help = "command Information"
+
+    def handle(self, *args, **kwargs):
+        fake = Faker()
+        # fake = Faker("nl_NL") #Specify Language
+
+        # print(faker.name())
+        fake.add_provider(Provider)
+        # print(fake.fake_content())
+
+        for _ in range(15):
+            # d = fake.unique.fake_content()
+            title = fake.product_title()
+            content = fake.product_content()
+            price = fake.price()
+            Product.objects.create(title = title, content = content, price = price)
+        self.stdout.write(self.style.SUCCESS(f" Number of product: { Product.objects.all().count() }"))
+
+        
