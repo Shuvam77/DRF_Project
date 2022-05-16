2f329595d7d5d0a0751125ed5a04361c86f72dfb
shuvam77 <shuvam77@gmail.com>
2022-05-16 18:36:19 +0200

    update and delete api

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -3,6 +3,13 @@ from . import views
 
 
 urlpatterns = [
-    path('<int:pk>/',views.ProductDetailAPIView.as_view()),
     path('', views.ProductListCreateAPIView.as_view()),
+    path('<int:pk>/',views.ProductDetailAPIView.as_view()),
+    path('<int:pk>/update/', views.ProductUpdateAPIView.as_view()),
+    path('<int:pk>/delete/', views.ProductDeleteAPIView.as_view()),
+
+    # path('<int:pk>/',views.product_alt_view),
+    # path('', views.product_alt_view),
+
+
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,8 +1,12 @@
-from yaml import serialize
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 from rest_framework import generics
 
 from .models import Product
 from .serializers import ProductSerializer
+from django.shortcuts import get_object_or_404
+# from django.http import Http404
 
 # Create your views here.
 
@@ -11,6 +15,27 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     #lookup_field = "pk"
+
+
+class ProductUpdateAPIView(generics.UpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        if not instance.content:
+            instance.content = "Add Something Later On"
+
+
+class ProductDeleteAPIView(generics.DestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+    def perform_destroy(self, instance):
+        super().perform_destroy(instance)
+
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
@@ -41,3 +66,32 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
 # class ProductListAPIView(generics.ListAPIView):
 #     queryset = Product.objects.all()
 #     serializer_class = ProductSerializer
+
+
+#function based view
+@api_view(['GET', 'POST'])
+def product_alt_view(request, pk=None, *args, **kwargs):
+    method = request.method
+
+    if method == "GET":
+        if pk is not None:
+            #DetailView
+            # queryset = Product.objects.filter(pk=pk)
+            queryset = get_object_or_404(Product, pk=pk)
+            data = ProductSerializer(queryset, many=False).data
+            return Response(data)
+        #ListView
+        queryset = Product.objects.all()
+        data = ProductSerializer(queryset, many = True).data
+        return Response(data)
+    if method == "POST":
+        serializer = ProductSerializer(data = request.data)
+        if serializer.is_valid(raise_exception=True):
+            title = serializer.validated_data.get('title')
+            content = serializer.validated_data.get('content') or None
+            if content is None:
+                content = "add content later on!"
+            serializer.save(content=content)
+            return Response(serializer.data)
+        # return Response({"invalid": "not good data title is missing"}, status=400)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
