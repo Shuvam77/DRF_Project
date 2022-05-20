8b900c49f298593e267025e84deae2886e3d4716
shuvam77 <shuvam77@gmail.com>
2022-05-20 17:53:16 +0200

    foreign key serializer

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,18 +1,22 @@
+from requests import request
 from rest_framework import serializers
 from rest_framework.reverse import reverse
 from .models import Product
 from .validators import validate_title, validate_title_no_suchvalue, unique_product_title
-
+from api.serializers import UserPublicSerializer
 
 class ProductSerializer(serializers.ModelSerializer):
+    owner = UserPublicSerializer(source ='user', read_only=True)
+    my_user_data = serializers.SerializerMethodField(read_only = True)
     my_discount = serializers.SerializerMethodField(read_only=True)
     url_edit = serializers.SerializerMethodField(read_only=True)
     url = serializers.HyperlinkedIdentityField(view_name="product-detail", lookup_field = "pk")
     title = serializers.CharField(validators=[validate_title_no_suchvalue, unique_product_title])
+    # email = serializers.EmailField(source='user.email', read_only=True)
     # email = serializers.EmailField(write_only = True)
     class Meta:
         model = Product
-        fields = ['pk', 'url', 'url_edit', 'title', 'content', 'price', 'sale_price', 'my_discount']
+        fields = ['owner', 'pk', 'url', 'url_edit', 'title', 'content', 'price', 'sale_price', 'my_discount', 'my_user_data']
 
     # validationFunction
     # def validate_title(self, value):
@@ -50,3 +54,6 @@ class ProductSerializer(serializers.ModelSerializer):
         if request is None:
             return None
         return reverse("product-edit", kwargs={"pk":obj.pk}, request=request)
+
+    def get_my_user_data(self, obj):
+        return obj.user.username
